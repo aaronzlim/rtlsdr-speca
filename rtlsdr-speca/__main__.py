dc65e8fb72f4490b0f4b6e74b6975305cdb96fc5
Aaron Lim <aaronzlim@gmail.com>
2021-01-01 11:34:10 -0500

    Initial commit

---
+++ rtlsdr-speca/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy.fft import fft, fftshift, fftfreq
+from rtlsdr import RtlSdr
+import PySimpleGUI as sg
+
+if __name__ == '__main__':
+    pass
